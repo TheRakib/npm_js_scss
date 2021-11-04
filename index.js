a6b8da49b4582ba83b5dd901c05490171454b7c8
RakibTest <rakibmedieinstitutet@gmail.com>
2021-11-04 11:23:34 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// Ladda ner nodejs om ni inte har gjort det
+// skapa ett nytt projektmapp
+// lägg till i terminalen: npm init -y
+//npm namn :- småbokstäver till projektnamn
+
+
+//console.log("node console ", process)
+// 11.00
+console.log("hello från node")
+
+// paket/dependencies /devdependencies 
+//node-sass
+// npm install node-sass
+// npm i node-sass
+
+
+// run commands
+
+
+// 10.15
